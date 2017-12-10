0f071b47e0385ff4aa4ca2287200e31069fec11e
norric <Riwzi@users.noreply.github.com>
2017-12-10 16:21:26 +0100

    add initial gui

---
+++ src/AppStates/Menu.java
@@ -0,0 +1,139 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package AppStates;
+
+/**
+ *
+ * @author Rickard
+ */
+
+import Network.GameClient.GameClient;
+import Network.GameServerLite;
+import com.jme3.app.Application;
+import com.jme3.app.state.BaseAppState;
+import com.jme3.niftygui.NiftyJmeDisplay;
+import de.lessvoid.nifty.Nifty;
+import de.lessvoid.nifty.NiftyEventSubscriber;
+import de.lessvoid.nifty.controls.*;
+import de.lessvoid.nifty.screen.Screen;
+import de.lessvoid.nifty.screen.ScreenController;
+import java.util.Collection;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.LinkedBlockingQueue;
+
+public class Menu extends BaseAppState implements ScreenController {
+
+    //TODO PROPERLY ADD ASSETS, IT CANT FIND THE START-BACKGROUND (but why can it find the xml files?!?)
+    //TODO Listen to the playername textfield, currently writing there doesnt do anything
+    
+    private Nifty nifty;
+    private NiftyJmeDisplay niftyDisplay;
+    private GameClient sapp;
+    
+    private GameServerLite currentServer;
+    
+    @Override
+    public void initialize(Application app) {
+        System.out.println("Menu: initialize");
+        sapp = (GameClient) app;
+    }
+    
+    @Override
+    protected void cleanup(Application app) {
+        System.out.println("Menu: cleanup");
+    }
+    
+    @Override
+    protected void onEnable() {
+        System.out.println("Menu: onEnable");
+        niftyDisplay = NiftyJmeDisplay.newNiftyJmeDisplay(
+                sapp.getAssetManager(),
+                sapp.getInputManager(),
+                sapp.getAudioRenderer(),
+                sapp.getGuiViewPort());
+        nifty = niftyDisplay.getNifty();
+        nifty.fromXml("Interface/scene.xml", "start", this);
+
+        // attach the nifty display to the gui view port as a processor
+        sapp.getGuiViewPort().addProcessor(niftyDisplay);
+        sapp.getInputManager().setCursorVisible(true);
+        
+        populateServerbrowser(new LinkedBlockingQueue<GameServerLite>()); //Only here for testing purposes
+        
+    }
+    
+    public void populateServerbrowser(Collection<GameServerLite> servers) {
+        ListBox<GameServerLite> listBox = (ListBox<GameServerLite>) nifty.getCurrentScreen().findNiftyControl("serverbrowser", ListBox.class);
+        if (listBox != null) {
+            listBox.clear();
+            listBox.addAllItems(servers);
+            listBox.addItem(new GameServerLite("127.0.0.1", "Castle",  8000,  4, 0, 2, 3)); //Hardcoded servers for testing
+            listBox.addItem(new GameServerLite("127.0.0.1", "Beach",   8000,  1, 1, 4, 3));
+            listBox.addItem(new GameServerLite("127.0.0.1", "Football", 8000, 0, 0, 2, 0));
+        }
+    }
+
+    /**
+     * When the selection of the ListBox changes this method is called.
+     */
+    @NiftyEventSubscriber(id="serverbrowser")
+    public void onMyListBoxSelectionChanged(final String id, final ListBoxSelectionChangedEvent<GameServerLite> event) {
+        List<GameServerLite> selection = event.getSelection();
+        for (GameServerLite selectedItem : selection) {
+            currentServer = selectedItem;
+        }
+    }
+    
+    public void quitGame() {
+        System.out.println("Menu: Quitting game!");
+        sapp.stop();
+    }
+    
+    public void joinServer() {
+        if (currentServer != null) {
+            sapp.joinServer(currentServer);
+        } else {
+            System.out.println("No server selected");
+        }
+    }
+    
+    public void refreshServerbrowser() {
+        sapp.queueRefreshMessage();
+    }
+    
+    @Override
+    protected void onDisable() {
+        System.out.println("Menu: onDisable");
+        sapp.getGuiViewPort().removeProcessor(niftyDisplay);
+        sapp.getInputManager().setCursorVisible(false);
+    }
+    
+    @Override
+    public void update(float tpf) {
+        //TODO: implement behavior during runtime
+    }
+
+    @Override
+    public void bind(Nifty nifty, Screen screen) {
+        System.out.println("bind( " + screen.getScreenId() + ")");
+    }
+
+    @Override
+    public void onStartScreen() {
+        System.out.println("onStartScreen");
+    }
+
+    @Override
+    public void onEndScreen() {
+        System.out.println("onEndScreen");
+    }
+
+    public void quit(){
+        nifty.gotoScreen("end");
+    }
+
+}
--- src/Network/GameClient/GameClient.java
+++ src/Network/GameClient/GameClient.java
@@ -8,15 +8,13 @@ package Network.GameClient;
 import Network.GameServerLite;
 import Network.Util;
 import com.jme3.app.SimpleApplication;
-import com.jme3.input.controls.ActionListener;
 import com.jme3.network.Client;
 import com.jme3.network.ClientStateListener;
 import com.jme3.network.Network;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.concurrent.Callable;
 import java.util.concurrent.LinkedBlockingQueue;
 import Network.Util.*;
+import AppStates.Menu;
 import com.jme3.network.Message;
 import java.util.Collection;
 
@@ -45,12 +43,17 @@ public class GameClient extends SimpleApplication implements ClientStateListener
     // Serverlist
     private Collection<GameServerLite> servers;
     
+    // AppStates
+    private Menu menu = new Menu();
+    
     private final String hostname; // where the authentication server can be found
     private final int port; // the port att the server that we use
     
     public GameClient(String hostname, int port) {
         this.hostname = hostname;
         this.port = port;
+        
+        this.stateManager.attach(menu);
     }
     
     public static void main(String[] args) {
@@ -62,6 +65,8 @@ public class GameClient extends SimpleApplication implements ClientStateListener
     public void simpleInitApp() {
         setDisplayStatView(false);
         setDisplayFps(false);
+        this.menu.setEnabled(true);
+        
         try {
             //Initialize the queue to use to send informations
             outgoingAuth = new LinkedBlockingQueue<>();
@@ -90,9 +95,55 @@ public class GameClient extends SimpleApplication implements ClientStateListener
     }
 
     public void setServerList(Collection<GameServerLite> servers) {
-        this.servers = servers;
+        if (menu.isEnabled()) {
+            menu.populateServerbrowser(servers);
+        }
     }
     
+    public void joinServer(GameServerLite server) {
+        if (gameConnection != null) {
+            gameConnection.close();
+            //TODO close the old senderThread here
+        }
+        try {           
+            System.out.println("Connect to a game server");
+            //Initialize the queue to use to send informations
+            outgoingGame = new LinkedBlockingQueue<>();
+            
+            gameConnection = Network.connectToServer(server.getAddress(), server.getPort());
+            this.gameSender = new GameClientSender(gameConnection, outgoingGame);
+
+            //Setup the listener for the game server
+            gameListener = new GameClientListener(gameConnection, this);
+            gameConnection.addMessageListener(authListener,
+                JoinAckMessage.class,
+                LobbyInformationMessage.class,
+                GameInformationMessage.class);
+            
+            // finally start the communication channel to the server
+            gameConnection.addClientStateListener(this);
+            gameConnection.start();
+            new Thread(gameSender).start();
+            outgoingAuth.put(new Util.RefreshMessage());
+        } catch (IOException ex) {
+            ex.printStackTrace();
+            this.destroy();
+            this.stop();
+        } catch (InterruptedException ex) {
+            ex.printStackTrace();
+        }
+    }
+    
+    public void queueRefreshMessage() {
+        System.out.println("Queueing refresh message to auth server");
+        try {
+            if (outgoingAuth != null) {
+                outgoingAuth.put(new RefreshMessage());   
+            }
+        } catch (InterruptedException ex) {
+            ex.printStackTrace();
+        }
+    }
     
     @Override
     public void simpleUpdate(float tpf) {
@@ -102,8 +153,12 @@ public class GameClient extends SimpleApplication implements ClientStateListener
     // takes down all communication channels gracefully, when called
     @Override
     public void destroy() {
-        authConnection.close();
-        gameConnection.close();
+        if (authConnection != null) {
+            authConnection.close();
+        }
+        if (gameConnection != null) {
+            gameConnection.close();
+        }
         super.destroy();
     }
     
