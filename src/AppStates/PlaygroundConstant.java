3c1c08bb7c3039dc3ee115bbbdd8bb9f082dc587
QuentinDel <quentindelooz@gmail.com>
2017-12-15 11:00:06 +0100

    GrassBoard creation

--- src/AppStates/PlaygroundConstant.java
+++ src/AppStates/PlaygroundConstant.java
@@ -11,19 +11,27 @@ package AppStates;
  */
 public interface PlaygroundConstant {
     
+    //Limitation playground
     static final float LINE_WIDTH = 75;
     static final float LINE_LENGTH = 125;
     static final float LINE_HEIGHT = 0.01f;
     static final float LINE_THICKNESS = 1;
+    static final float MIDDLE_DOT_RADIUS = 3;
     
-    static final float GOAL_LENGTH = LINE_WIDTH * 0.55f;
-    static final float GOAL_WIDTH = LINE_LENGTH * 0.2f;
-    static final float POSITION_GOAL_SIDE_LINE = LINE_LENGTH - GOAL_WIDTH;
-    static final float POSITION_GOAL_SIDE_LINE_WIDTH = GOAL_LENGTH; 
-    static final float POSITION_GOAL_LONG = POSITION_GOAL_SIDE_LINE - GOAL_WIDTH + LINE_THICKNESS;
+    //Area of goalkeeper zone
+    static final float GOAL_LINE_LENGTH = LINE_WIDTH * 0.55f;
+    static final float GOAL_LINE_WIDTH = LINE_LENGTH * 0.2f;
+    static final float POSITION_GOAL_SIDE_LINE = LINE_LENGTH - GOAL_LINE_WIDTH;
+    static final float POSITION_GOAL_SIDE_LINE_WIDTH = GOAL_LINE_LENGTH; 
+    static final float POSITION_GOAL_LONG = POSITION_GOAL_SIDE_LINE - GOAL_LINE_WIDTH + LINE_THICKNESS;
     
     static final float BOARD_WIDTH = LINE_WIDTH - LINE_THICKNESS;
     static final float BOARD_LENGTH = LINE_LENGTH - LINE_THICKNESS;
     
+    //Goal zone
+    static final float GOAL_TRANSV = GOAL_LINE_LENGTH * 0.6f;
+    static final float GOAL_PICKET_HEIGHT = 6f;
+    static final float GOAL_PICKET_THICKNESS = 1f;
+
     
 }
--- src/AppStates/TestPlayground.java
+++ src/AppStates/TestPlayground.java
@@ -7,8 +7,6 @@ package AppStates;
 
 import static AppStates.PlaygroundConstant.BOARD_LENGTH;
 import static AppStates.PlaygroundConstant.BOARD_WIDTH;
-import static AppStates.PlaygroundConstant.GOAL_LENGTH;
-import static AppStates.PlaygroundConstant.GOAL_WIDTH;
 import static AppStates.PlaygroundConstant.LINE_HEIGHT;
 import static AppStates.PlaygroundConstant.LINE_LENGTH;
 import static AppStates.PlaygroundConstant.LINE_THICKNESS;
@@ -33,18 +31,22 @@ import com.jme3.terrain.heightmap.ImageBasedHeightMap;
 import com.jme3.texture.Texture;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import static AppStates.PlaygroundConstant.GOAL_LINE_LENGTH;
+import static AppStates.PlaygroundConstant.GOAL_LINE_WIDTH;
+import com.jme3.scene.shape.Cylinder;
+import com.jme3.util.SkyFactory;
 
 /**
  *
  * @author Quentin
  */
 
-public class GrassPlayground extends SimpleApplication implements PlaygroundConstant {
+public class TestPlayground extends SimpleApplication implements PlaygroundConstant {
 
   private Node board;
 
   public static void main(String[] args) {
-    GrassPlayground app = new GrassPlayground();
+    TestPlayground app = new TestPlayground();
     app.start();
   }
 
@@ -65,13 +67,24 @@ public class GrassPlayground extends SimpleApplication implements PlaygroundCons
     Geometry leftLine = new Geometry("line", lengthLine);
     Box widthLine = new Box(LINE_THICKNESS, LINE_HEIGHT, LINE_WIDTH);
     Geometry topLine = new Geometry("line", widthLine);
+    Cylinder cylinderMesh = new Cylinder(32, 32, MIDDLE_DOT_RADIUS, 0.01f, true, false);
+    Geometry middleDot = new Geometry("Cylinder", cylinderMesh);
     
     //Geometry for the goal keeper zone
     Node goalZoneNode = new Node("Goal");
-    Box goalLine = new Box(GOAL_WIDTH, LINE_HEIGHT, LINE_THICKNESS);
+    Box goalLine = new Box(GOAL_LINE_WIDTH, LINE_HEIGHT, LINE_THICKNESS);
     Geometry goalWidth  = new Geometry("line", goalLine);
-    Box goalLineSide = new Box(LINE_THICKNESS, LINE_HEIGHT, GOAL_LENGTH);
+    Box goalLineSide = new Box(LINE_THICKNESS, LINE_HEIGHT, GOAL_LINE_LENGTH);
     Geometry goalLong = new Geometry("line", goalLineSide);
+    
+    //Geometry for the goal
+    Node goal = new Node("Goal");
+    Box picket = new Box(GOAL_PICKET_THICKNESS, GOAL_PICKET_HEIGHT, GOAL_PICKET_THICKNESS);
+    Geometry goalPicket = new Geometry("picket", picket);
+    Box trans = new Box(GOAL_TRANSV, GOAL_PICKET_THICKNESS, GOAL_PICKET_THICKNESS);
+    Geometry goalTrans = new Geometry("trans", trans);
+    
+    
     
     
     //Material used for the line
@@ -80,20 +93,31 @@ public class GrassPlayground extends SimpleApplication implements PlaygroundCons
     
     leftLine.setMaterial(matLine);
     topLine.setMaterial(matLine);
+    middleDot.setMaterial(matLine);
     goalWidth.setMaterial(matLine);
     goalLong.setMaterial(matLine);
+    goalPicket.setMaterial(matLine);
+    goalTrans.setMaterial(matLine);
+   
     
     Geometry rightLine = leftLine.clone(true);
     Geometry bottomLine = topLine.clone(true);
+    Geometry middleLine = topLine.clone(true);
     Geometry goalWidthOtherSide = goalWidth.clone(true);
+    Geometry otherPicket = goalPicket.clone(true);
             
     linesNode.attachChild(leftLine);
     linesNode.attachChild(rightLine);
     linesNode.attachChild(topLine);
     linesNode.attachChild(bottomLine);
+    linesNode.attachChild(middleLine);
+    linesNode.attachChild(middleDot);
     goalZoneNode.attachChild(goalWidth);
     goalZoneNode.attachChild(goalLong);
     goalZoneNode.attachChild(goalWidthOtherSide);
+    goal.attachChild(goalPicket);
+    goal.attachChild(goalTrans);
+    goal.attachChild(otherPicket);
 
     
     //Set at the right place
@@ -101,18 +125,32 @@ public class GrassPlayground extends SimpleApplication implements PlaygroundCons
     rightLine.setLocalTranslation(0f, 0f, BOARD_WIDTH);
     topLine.setLocalTranslation(BOARD_LENGTH, 0f, 0f);
     bottomLine.setLocalTranslation(-BOARD_LENGTH, 0f, 0f);
+    middleDot.rotate( 90*FastMath.DEG_TO_RAD , 0f,0f);
     goalWidth.setLocalTranslation(POSITION_GOAL_SIDE_LINE, 0f,POSITION_GOAL_SIDE_LINE_WIDTH);
     goalWidthOtherSide.setLocalTranslation(POSITION_GOAL_SIDE_LINE, 0f, -POSITION_GOAL_SIDE_LINE_WIDTH);
     goalLong.setLocalTranslation(POSITION_GOAL_LONG, 0f, 0f);
+    goalPicket.setLocalTranslation(BOARD_LENGTH, GOAL_PICKET_HEIGHT, GOAL_TRANSV - GOAL_PICKET_THICKNESS);
+    otherPicket.setLocalTranslation(BOARD_LENGTH, GOAL_PICKET_HEIGHT, -GOAL_TRANSV + GOAL_PICKET_THICKNESS);
+    goalTrans.setLocalTranslation(BOARD_LENGTH, 2*GOAL_PICKET_HEIGHT, 0f);
+    goalTrans.rotate(0f , 90*FastMath.DEG_TO_RAD, 0f);
     
-    Node otherGoal = goalZoneNode.clone(true);
-    otherGoal.rotate(0f , 0f , 180*FastMath.DEG_TO_RAD);
+    Node otherGoalZone = goalZoneNode.clone(true);
+    otherGoalZone.rotate(0f , 0f , 180*FastMath.DEG_TO_RAD);
+    Node otherGoal = goal.clone(true);
+    otherGoal.rotate(0f , 180*FastMath.DEG_TO_RAD , 0f);
+    //otherGoal.getChild("trans").rotate(180*FastMath.DEG_TO_RAD , 0f , 0f);
 
     board = new Node("board");
     board.attachChild(linesNode);
     board.attachChild(playgroundNode);
     board.attachChild(goalZoneNode);
+    board.attachChild(otherGoalZone);
+    board.attachChild(goal);
     board.attachChild(otherGoal);
+    board.attachChild(SkyFactory.createSky(getAssetManager(), "Textures/Sky/Bright/BrightSky.dds", SkyFactory.EnvMapType.CubeMap));
+    //System.out.println(otherGoal.getChild("trans").getLocalTranslation());
+    //System.out.println(goal.getChild("trans").getLocalTranslation());
+    
     
     rootNode.attachChild(board);
   }
