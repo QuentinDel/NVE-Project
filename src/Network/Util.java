baf3abb7f381aed87fda7dd76652df22d70f9428
norric <Riwzi@users.noreply.github.com>
2018-01-08 11:42:38 +0100

    change hostnames for testing

--- src/Network/Util.java
+++ src/Network/Util.java
@@ -20,8 +20,8 @@ import java.util.concurrent.ConcurrentHashMap;
 
 public class Util {
     
-    //public static final String HOSTNAME = "130.240.156.217";
-    public static final String HOSTNAME = "127.0.0.1";
+    public static final String HOSTNAME = "130.240.153.60";
+    public static final String GAME_HOSTNAME = "130.240.155.53";
     
     public static final int PORT = 7006;
     public static final int PORT_GAME = 7007;
