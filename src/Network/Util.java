387a4656a136d4f45ae0f9fdb3e0985fada28b7b
norric <Riwzi@users.noreply.github.com>
2018-01-09 15:01:53 +0100

    change IPs for testing

--- src/Network/Util.java
+++ src/Network/Util.java
@@ -20,7 +20,7 @@ import java.util.concurrent.ConcurrentHashMap;
 
 public class Util {
     
-    public static final String HOSTNAME = "130.240.108.113";
+    public static final String HOSTNAME = "130.240.152.76";
     public static final String GAME_HOSTNAME = "130.240.153.91";
     //public static final String HOSTNAME = "127.0.0.1";
     //public static final String GAME_HOSTNAME = "127.0.0.1";
